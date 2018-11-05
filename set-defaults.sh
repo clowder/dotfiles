ab5dc5accf0abfea01e90cc30cb3fc01ea92bc74
Chris Lowder <clowder@users.noreply.github.com>
2018-11-05 11:33:43 +0000

    Remove insanely fast key repeat setting

--- set-defaults.sh
+++ set-defaults.sh
@@ -15,9 +15,6 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
-
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
