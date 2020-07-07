4aee90d901f3d32dd7558037b2cd73708674240a
Chris Lowder <clowder@gmail.com>
2020-07-07 12:28:03 +0200

    Remove SSD optimisations
    These are from a time before SSDs were the default for Macs and aren't
    needed anymore.

--- set-defaults.sh
+++ set-defaults.sh
@@ -62,17 +62,3 @@ defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
-
-###############################################################################
-# SSD-specific tweaks                                                         #
-###############################################################################
-
-# Disable hibernation (speeds up entering sleep mode)
-sudo pmset -a hibernatemode 0
-
-# Remove the sleep image file to save disk space
-sudo rm /private/var/vm/sleepimage
-# Create a zero-byte file instead…
-sudo touch /private/var/vm/sleepimage
-# …and make sure it can’t be rewritten
-sudo chflags uchg /private/var/vm/sleepimage
